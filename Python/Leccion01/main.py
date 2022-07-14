df8a97a0d92861c387c6f10cb18b0c518c278f76
Nicolas <nicolasdelponte2012@gmail.com>
2022-07-14 00:38:33 -0300

    Leccion 01 subida

---
+++ Python/Leccion01/main.py
@@ -0,0 +1,129 @@
+'''
+#TENER EN CUENTA EL DOCUMENTO A CONTINUACION ESTA GUARDADO CON LA EXTENSION .py(Python) Y SE ENCUENTRA COMENTADO
+#PARA PODER REVISARLO POR PARTES.
+
+
+print("¡Hola mundo python!")
+print("¡Este es mi saludos desde Python!")
+print('¡._.xD!')
+#Este es un commentario ¡._.xD!
+#SE PUEDEN USAR ("") u ('') ¡._.xD!
+
+#VARIABLES
+#VARIABLES
+#VARIABLES
+
+miVariable = "Hola mundo desde Python en VARIABLES"
+#varialbe sola
+print(miVariable)
+# espacio
+print("")
+#Concatenacion de variable con texto
+print("Variable(miVariable): "+miVariable)
+
+# espacio
+print("")
+miVariable = 4
+#varialbe sola
+print(miVariable)
+# espacio
+print("")
+# Concatenacion de variable con texto (no posible asi nomas)
+# Error : print("Variable(miVariable): "+miVariable)
+# Traceback (most recent call last):
+#  File "D:\AprendiendoPython\Python\Leccion01\main.py", line 24, in <module>
+#   print("Variable(miVariable): "+miVariable)
+# TypeError: can only concatenate str (not "int") to str
+
+
+x = 10
+y = 2
+z = x+y #por lo tanot seria 10
+
+print(x)
+print("")
+print(y)
+print("")
+print(z)
+print("")
+
+print(id(x))
+
+print(id(y))
+
+print(id(z))
+# 3 varianbles
+print("")
+print("")
+print("")
+
+
+#comentarios de punto a punto '''                   '''
+'''                   '''
+#comentarios de punto a punto '''                   '''
+
+
+Nombre = "Nicolas Delponte"
+x = 1560054285
+Mail = "nicolasdelponte2012@gmail.com"
+#print (funcion)
+# () parametro
+print(Nombre)
+print(x)
+print(Mail)
+# lAS VARIABLES SON CASE SENSITIVE
+
+#VARIABLES EOF
+#VARIABLES EOF
+#VARIABLES EOF
+
+#TIPOS DE DATOS
+#TIPOS DE DATOS
+#TIPOS DE DATOS
+
+x = 20
+print(x)
+print("Tipo de dato: ")
+print(type(x))
+
+
+x = "Hola mundo en Python"
+print(x)
+print("Tipo de dato: ")
+print(type(x))
+
+x: str = "Hola mundo en Python!"
+print(x)
+print("Tipo de dato: ")
+print(type(x))
+
+## ATENTO AL PROXIMO RENGLON CON LA ANOTACION STR
+x: str = 20
+print(x)
+print("Tipo de dato: ")
+print(type(x))
+
+x = 10.5
+print(x)
+print("Tipo de dato: ")
+print(type(x))
+
+
+x = True
+print(x)
+print("Tipo de dato: ")
+print(type(x))
+
+x = False
+print(x)
+print("Tipo de dato: ")
+print(type(x))
+
+
+#TIPOS DE DATOS EOF
+#TIPOS DE DATOS EOF
+#TIPOS DE DATOS EOF
+
+#QUEDAMOS EN CLASE 13 SECCION 3.
+'''
+
